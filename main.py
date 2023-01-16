435cb26875e1730da9e2edc960e1e4f78c76e3ad
SaraAzimiGit <87041046+SaraAzimiGit@users.noreply.github.com>
2023-01-16 17:03:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,59 +3,144 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import pandas as pd
 import streamlit as st
-import numpy as np
-import time
-st.title('Occupancy detection selection tool')
-
-q1=st.text_input("Are there energy code compliance requirements? (Yes/No)")
-
-if q1 in ["No","NO","no"]:
-    q2 = st.text_input("Would you like occupancy based controls? (Yes/No)")
-    if q2 in ["No","NO","no"]:
-        st.write("Other control strategies are more appropriate")
-    if q2 in ["Yes","YES","yes"]:
-        q3 = st.text_input("Does space contain partitions, large equipment or furniture?")
-if q1 in ["Yes", "YES", "yes"]:
-        q3 = st.text_input("Does space contain partitions, large equipment or furniture?")
-        if q3 in ["No", "NO", "no"]:
-            st.write("Cameras/vision sensors, PIR, break beam sensors")
-        if q3 in ["Yes", "YES", "yes"]:
-            q4 = st.text_input("Is space small or well defined?")
-            if q4 in ["Yes", "YES", "yes"]:
-                q5 = st.text_input("Is there a clear line of sight to all areas?")
-                if q5 in ["No","NO","no"]:
-                    st.write("Radio frequency-based technology, ultrasonic and smart meters ")
-                if q5 in ["Yes", "YES", "yes"]:
-                    q6 = st.text_input("Would installation of additional sensors justify payback?")
-                    if q6 in ["Yes", "YES", "yes"]:
-                        st.write("Cameras/vision sensors, PIR, break beam sensors, electro-mechanical sensors")
-                    if q6 in ["No","NO","no"]:
-                        st.write("Wi-Fi, Building data")
-            if q4 in ["No","NO","no"]:
-                q7 = st.text_input("Are there definite space boundaries?")
-                if q7 in ["No", "NO", "no"]:
-                    st.write("Ultrasonic, microwave, radio frequency-based technology")
-                if q7 in ["Yes", "YES", "yes"]:
-                    q8 = st.text_input("Is there a high volume of air flow and non-human heat sources?")
-                    if q8 in ["Yes", "YES", "yes"]:
-                        q9 = st.text_input("Can owners choose an appropriate mounting location away from air flow?")
-                        if q9 in ["Yes", "YES", "yes"]:
-                            st.write("Ultrasonic, microwave, PIR, infrared camera, acoustic and CO2 sensor")
-                        if q9 in ["No", "NO", "no"]:
-                            st.write("""Radio frequency-based technology, TOF, Binocular SL depth and optical cameras, 
-                            smart meters, electro-mechanical sensors""")
-                    if q8 in ["No","NO","no"]:
-                        q10 = st.text_input("Is there moving mechanical equipment in the space?")
-                        if q10 in ["No", "NO", "no"]:
-                            st.write("""Radio frequency-based technology, TOF, Binocular SL depth and optical cameras, 
-                            smart meters, electro-mechanical sensors""")
-                        if q10 in ["Yes", "YES", "yes"]:
-                            st.write("Radio frequency-based technology, smart meters")
+import pandas as pd
+from itertools import combinations
 
 
+@st.cache
+def r_subset(arr, r):
+    return list(combinations(arr,r))
+
+def convert(string):
+    li = list(string.split(",", ))
+    return li
+
+def main():
+    siteHeader = st.beta_container()
+    appSelection = st.beta_container()
+    with siteHeader:
+        st.title('Occupancy detection technology screening tool')
+        st.text("""With this tool you can screen for the best occupancy detection options 
+that suits your facility management application needs""")
+
+    with appSelection:
+        info_app = {'Occupancy Resolution': [4, 4, 2, 3, 3, 3, 2, 1, 2, 1, 1, 2, 2, 2],
+                    'Spatial Resolution': [3, 3, 2, 3, 3, 2, 3, 2, 2, 3, 4, 2, 2, 2],
+                    'Accuracy': [3, 3, 2, 3, 3, 3, 2, 1, 1, 3, 2, 1, 1, 1]}
+        info_app_1 = {
+            'Occupancy_Resolution': ['Tracking', 'Tracking', 'Count', 'Identity', 'Identity', 'Identity', 'Count',
+                                     'Presence', 'Count', 'Presence', 'Presence', 'Count', 'Count', 'Count'],
+            'Spatial_Resolution': ['Room', 'Room', 'Floor', 'Room', 'Room', 'Floor', 'Room', 'Floor', 'Floor', 'Room',
+                                   'Workstation', 'Floor', 'Floor', 'Floor'],
+            'Accuracy': ['High', 'High', 'Medium', 'High', 'High', 'High', 'Medium', 'Low', 'Low', 'High', 'Medium',
+                         'Low', 'Low', 'Low']}
+        columns = ['Occupancy Resolution', 'Spatial Resolution', 'Accuracy']
+        df = pd.DataFrame(data=info_app, columns=columns,
+                          index=['Emergency evacuation and rescue', 'Disease control', 'Space utilization rate',
+                                 'Access control', 'Intrusion detection',
+                                 'Surveillance', 'Smart cleaning', 'Heating/cooling control',
+                                 'Ventilation control', 'Lighting control', 'Plug load control',
+                                 'Heating/cooling energy usage', 'Lighting energy usage', 'Plug load energy usage'])
+        df_1 = pd.DataFrame(data=info_app_1, columns=columns,
+                            index=['Emergency evacuation and rescue', 'Disease control', 'Space utilization rate',
+                                   'Access control', 'Intrusion detection', 'Surveillance', 'Smart cleaning',
+                                   'Heating/cooling control',
+                                   'Ventilation control', 'Lighting control', 'Plug load control',
+                                   'Heating/cooling energy usage', 'Lighting energy usage', 'Plug load energy usage'])
+        info_sensor_0 = {
+            'Sensor': ['PIR/Break beam/Ultrasonic/Microwave', 'TOF/Binocular/SL/Infrared camera',
+                       'Optical camera', 'Wi-Fi', 'RFID tag/UWB/Bluetooth', 'Acoustic/Smart meters', 'Door', 'CO2',
+                       'Piezoelectric'],
+            'Occupancy Resolution': [1, 2, 3, 4, 4, 2, 1, 2, 1],
+            'Spatial Resolution': [4, 4, 4, 4, 3, 3, 3, 3, 3],
+            'Accuracy': [1, 3, 3, 3, 3, 2, 1, 1, 1],
+            'Privacy': [3, 3, 1, 1, 1, 3, 3, 3, 3],
+            'Cost': [1, 3, 3, 1, 2, 1, 1, 1, 1]}
+        info_sensor_1 = {
+            'Occupancy Resolution': ['Presence', 'Count', 'Identity', 'Tracking', 'Tracking', 'Count', 'Presence',
+                                     'Count', 'Presence'],
+            'Spatial Resolution': ['Workstation', 'Workstation', 'Workstation', 'Workstation', 'Room', 'Room', 'Room',
+                                   'Room', 'Room'],
+            'Accuracy': ['Low', 'High', 'High', 'High', 'High', 'Medium', 'Low', 'Low', 'Low'],
+            'Privacy': ['High', 'High', 'Low', 'Low', 'Low', 'High', 'High', 'High', 'High'],
+            'Cost': ['Low', 'High', 'High', 'Low', 'Medium', 'Low', 'Low', 'Low', 'Low']}
+        columns_sensor_0 = ['Sensor','Occupancy Resolution', 'Spatial Resolution', 'Accuracy', 'Privacy', 'Cost']
+        columns_sensor_1 = ['Occupancy Resolution', 'Spatial Resolution', 'Accuracy', 'Privacy', 'Cost']
+
+        df_0 = pd.DataFrame(data=info_sensor_0, columns=columns_sensor_0,
+                            index=['PIR/Break beam/Ultrasonic/Microwave', 'TOF/Binocular/SL/Infrared camera',
+                                   'Optical camera', 'Wi-Fi', 'RFID tag/UWB/Bluetooth', 'Acoustic/Smart meters', 'Door',
+                                   'CO2', 'Piezoelectric'])
+        df1_1 = pd.DataFrame(data=info_sensor_1, columns=columns_sensor_1,
+                             index=['PIR/Break beam/Ultrasonic/Microwave', 'TOF/Binocular/SL/Infrared camera',
+                                    'Optical camera', 'Wi-Fi', 'RFID tag/UWB/Bluetooth', 'Acoustic/Smart meters',
+                                    'Door', 'CO2', 'Piezoelectric'])
+        menu_items = ["View all sensors available", "Select single type of sensor based on application criteria",
+                      "Select combination of sensors based on application criteria"]
+        occ_res_cat=['N/A','Presence','Count','Identity','Tracking']
+        occ_res_num=[0,1,2,3,4]
+        df2_occ=pd.DataFrame(data=occ_res_num,index=occ_res_cat)
+        st.write(df2_occ)
+       # df3_occ=pd.DataFrame(data=occ_res_num)
+        with st.form(key="Selecting columns"):
+            q1 = st.multiselect('Select facility management application(s)', df_1.index)
+            menu_choices = st.selectbox('Select your sensor options', menu_items)
+            privacy_q=st.multiselect('What level of occupancy resolution is not allowed for data collection',df1_1['Occupancy Resolution'].unique())
+            submit_button = st.form_submit_button(label='Update')
+
+        if submit_button:
+            #res=df2_occ.loc[privacy_q]
+            st.write(privacy_q)
+
+            result_1 = df_1.loc[q1]
+            app_selected = pd.DataFrame(data=result_1, columns=columns)
+            app_selected.update(df)
+
+            max_occ_res = app_selected['Occupancy Resolution'].max()
+            max_spatial_res = app_selected['Spatial Resolution'].max()
+            max_acc = app_selected['Accuracy'].max()
+
+            min_occ_res = app_selected['Occupancy Resolution'].min()
+            min_spatial_res = app_selected['Spatial Resolution'].min()
+            min_acc = app_selected['Accuracy'].min()
+
+            if menu_choices == "View all sensors available":
+                st.write("Below are all the sensors Available")
+                st.dataframe(df1_1)
+            if menu_choices == "Select single type of sensor based on application criteria":
+                df_0 = df_0.drop(columns='Sensor')
+                filter_table1 = df_0.loc[
+                    (df_0['Occupancy Resolution'] >= max_occ_res) & (df_0['Accuracy'] >= max_acc) & (
+                            df_0['Spatial Resolution'] >= max_spatial_res)]
+                filter_table1.update(df1_1)
+                if filter_table1[filter_table1["Occupancy Resolution"].isin(privacy_q)]:
+                    st.dataframe(filter_table1)
+            if menu_choices == "Select combination of sensors based on application criteria":
+                min_table = df_0.loc[(min_occ_res <= df_0['Occupancy Resolution']) & (min_acc <= df_0['Accuracy']) & (
+                            min_spatial_res <= df_0['Spatial Resolution'])]
+                sensor = min_table.index
+                min_table = df_0.loc[(min_occ_res <= df_0['Occupancy Resolution']) & (min_acc <= df_0['Accuracy']) & (
+                            min_spatial_res <= df_0['Spatial Resolution'])]
+
+                for f in range(len(sensor) + 1):  # iterate number of items in combination
+                    if f >= 2:
+                        for i in r_subset(sensor, f):  # iterate over every combination
+                            is_ok=False
+                            for key in i:  # iterate over any item in a combination
+                                multi_row = min_table[min_table['Sensor'].isin(convert(key))]
+                                value_occ_res = multi_row['Occupancy Resolution'].item()
+                                value_spatial_res = multi_row['Spatial Resolution'].item()
+                                value_acc = multi_row['Accuracy'].item()
+                                if value_occ_res >= max_occ_res or value_spatial_res >= max_spatial_res or value_acc >= max_acc:  # pick any item in a  combination that meets the criteria
+                                    is_ok=True
+                            if is_ok==True:
+                                info = df_0.loc[i,:]
+                                info = info.drop(columns='Sensor')
+                                info.update(df1_1)
+                                st.dataframe(info)
+                                st.write(info['Occupancy Resolution'])
 
 
-
-
+if __name__ == "__main__":
+    main()
